0230be7d54d0338637566b338db94b3a180c8cff
wwc <wwcphy@terpmail.umd.edu>
2018-11-23 16:14:44 -0500

    copy grid.py from bem package and add to_xyz() function

--- electrode/__init__.py
+++ electrode/__init__.py
@@ -8,6 +8,7 @@ from .pattern_constraints import (PotentialObjective, PatternRangeConstraint,
         MultiPotentialObjective)
 from .transformations import euler_from_matrix, euler_matrix
 from .utils import shaped
+from .grid import Grid, UnstructuredGrid, SphericalGrid
 
 #from .constraints import (VoltageConstraint, SymmetryConstraint,
 #        PotentialConstraint, ForceConstraint, CurvatureConstraint,
